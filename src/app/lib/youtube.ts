933272d5b51ca9932ca029bae4c76b94ffc99d3f
José Viejo HUerta <Jviejo@users.noreply.github.com>
2024-08-15 11:06:42 +0200

    fix

--- src/app/lib/youtube.ts
+++ src/app/lib/youtube.ts
@@ -9,7 +9,8 @@ export async function getYoutubeTranscript(videoId: string) {
     console.log("cadena",pos2)
     const cadena = "https://" + data.substring(pos1,pos2)
     console.log("cadena",cadena)
-    const response1 = await fetch(cadena.replace(/\\u0026/g,"&"))
+    // const response1 = await fetch(cadena.replace(/\\u0026/g,"&"))
+    const response1 = await fetch("https://www.youtube.com/api/timedtext?v=LRfDAZfo00o&ei=Jbi9ZqzePLehp-oPjuvkmQs&caps=asr&opi=112496729&exp=xbt&xoaf=5&hl=es&ip=0.0.0.0&ipbits=0&expire=1723734678&sparams=ip,ipbits,expire,v,ei,caps,opi,exp,xoaf&signature=B703D0A6359F1467A9C08400031FE4BC5A4571FC.4E3F636DBDE6EDC2AD719763D5043607966CFC6D&key=yt8&kind=asr&lang=en")
     const data1 = await response1.text()
     return data1
   
